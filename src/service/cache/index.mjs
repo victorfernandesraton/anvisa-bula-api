8c94bd919e3ca0bb4590ba311047727dbe5a77ec
Victor Raton <vfbraton@gmail.com>
2022-07-19 22:35:25 -0300

    feat: optional cache in server

--- src/service/cache/index.mjs
+++ src/service/cache/index.mjs
@@ -4,9 +4,16 @@ import config from '../../config/index.mjs'
 
 const url = `redis://${config.cache.domain}:${config.cache.port}`
 
-export default createClient({
-	url: url,
-	password: config.cache.password,
-}).on('error', (err) => log('Redis Client Error', err))
-	.on('connect', () => log(`Cache connected with ${url}`))
-	.on('reconnect', () => log('Cache reconnected with ${url}'))
+let cache = null
+
+if (config.cache.domain) {
+
+	cache = createClient({
+		url: url,
+		password: config.cache.password,
+	}).on('error', (err) => log('Redis Client Error', err))
+		.on('connect', () => log(`Cache connected with ${url}`))
+		.on('reconnect', () => log('Cache reconnected with ${url}'))
+}
+
+export default cache
